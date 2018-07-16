d991b2836818a20dedd4e52e4cad5d4812ca1d55
Eeresh <esagar@seic.com>
2018-07-16 14:57:07 +0530

    First Commit Uploading All Files

---
+++ CourseApi/src/main/java/io/springbootframeworkstarter/hello/HelloController.java
@@ -0,0 +1,13 @@
+package io.springbootframeworkstarter.hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@RequestMapping("/hi")
+	public String sayHi(){
+		return "Hi Welocome to SpringBoot Tutorial";
+	}
+}
